ce7eb644afbb97a868ca9285ff04fd3cef0ba345
Alex Shmakov <titanaruto@gmail.com>
2020-02-07 13:42:06 +0200

    task 2 completed

--- lesson_1/task_2.py
+++ lesson_1/task_2.py
@@ -2,7 +2,22 @@
 # минуты и секунды и выведите в формате чч:мм:сс. Используйте
 # форматирование строк.
 
-seconds = int(input("Введите время в секундах: "))
+# seconds = int(input("Введите время в секундах: "))
+seconds = 50000
+minutes = int(seconds / 60)
+hours = int(seconds / 3600)
+minutes = minutes - (hours * 60)
+list_seconds = list(str(seconds))
+last_index = len(list_seconds) - 1
+pre_last_index = len(list_seconds) - 2
 
-print(seconds)
+seconds = f"{list_seconds[pre_last_index]}{list_seconds[last_index]}"
+if int(seconds) > 60:
+    seconds = int(seconds) - 60
 
+if hours < 10:
+    hours = f'0{hours}'
+if minutes < 10:
+    minutes = f'0{minutes}'
+
+print(f"время {hours}:{minutes}:{seconds}")
