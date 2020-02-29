97a62298766e145fcedb59b6b7084c0fb1423a99
Alex <titanaruto@gmail.com>
2020-02-29 20:28:37 +0200

    completed task_1

---
+++ task_1/task_1.py
@@ -0,0 +1,34 @@
+# 1. Создать класс TrafficLight (светофор) и определить у него один атрибут color (цвет) и метод running (запуск).
+# Атрибут реализовать как приватный. В рамках метода реализовать переключение светофора в режимы:
+# красный, желтый, зеленый.
+# Продолжительность первого состояния (красный) составляет 7 секунд, второго (желтый) — 2 секунды,
+# третьего (зеленый) — на ваше усмотрение.
+# Переключение между режимами должно осуществляться только в указанном порядке (красный, желтый, зеленый).
+# Проверить работу примера, создав экземпляр и вызвав описанный метод.
+import time
+
+
+class TrafficLight:
+    color = [("желтый", 2, 2), ("зеленый", 4, 3), ("красный", 7, 1)]
+    work = False
+
+    def __init__(self):
+        self.work = True
+        self._running()
+
+    def _get_work(self):
+        return self.work
+
+    def _running(self):
+        work_temp = self._get_work()
+        if work_temp:
+            sort_colors = sorted(self.color, key=lambda index: index[2])
+            while work_temp:
+                for element in sort_colors:
+                    color, time_sleep, *other = element
+                    print(f"Горит {color} цвет.")
+                    time.sleep(int(time_sleep))
+
+
+if __name__ == "__main__":
+    start = TrafficLight()
