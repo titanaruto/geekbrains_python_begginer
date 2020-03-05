1fd8a6fdb0eb03ee3dfc731eb1f8fc99c2fb5cc2
Alex Shmakov <titanaruto@gmail.com>
2020-03-05 18:55:04 +0200

    stub

--- task_5/task_5.py
+++ task_5/task_5.py
@@ -3,3 +3,22 @@
 # Проверьте работу проекта, создав экземпляры класса (комплексные числа)
 # и выполнив сложение и умножение созданных экземпляров.
 # Проверьте корректность полученного результата.
+
+
+class MyComplexNumber:
+
+    def __init__(self, number):
+        self._number = number
+
+    def __add__(self, other):
+        # sum_number = self._number
+        # return MyComplexNumber(sum_number)
+        pass
+
+    def __mul__(self, other):
+        pass
+
+
+if __name__ == '__main__':
+    number1 = MyComplexNumber("1+3i")
+    number2 = MyComplexNumber("4-5i")
